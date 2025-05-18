cd87f56c579817f40cef5b455da90b8847be80fc
K3SO <sergulaz05@gmail.com>
2025-05-18 14:44:40 +0200

    refactor: Los comandos ahora se lanzan con decoradores

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # BMO
 Es un bot de discord sencillo para una pequeña comunidad de amigos.
-En caso de usar este código para tu bot porfavor dar crédito en la biografía o en algún sitio similar.
+En caso de usar este código para tu bot, porfavor, da crédito en la biografía o en algún sitio relacionado.
 Se prohibe usar este código con fines lucrativos.
 
 ## Dependencias
-- dotenv-python
-- yt-dlp
-- (es posible que falte alguna más, pronto se confirmará)
+- discord
+- python-dotenv
--- src/main.py
+++ src/main.py
@@ -1,35 +1,37 @@
-from typing import Final
 import os
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from command_handler import get_response
+from typing import Final
 
 # Declara las variables de .env
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-PREFIX: Final[str] = os.getenv('CMMND_PREFIX')
+PREFIX: Final[str] = os.getenv('PREFIX')
+DEV_GUILD: Final[int] = os.getenv('DEV_GUILD')
 
-# Saca las dependencias necesarias
-intents: Intents = Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
-client: Client = Client(intents=intents)
 
-# Avisa por la consola que se ha conectado correctamente
+client = commands.Bot(command_prefix=PREFIX, intents=intents)
+GUILD_ID = discord.Object(id=DEV_GUILD)
+
 @client.event
-async def on_ready() -> None:
+async def on_ready():
     print(f'Bot conectado como {client.user}!\nPrefijo establecido como: "{PREFIX}"')
+    try:
+        guild = discord.Object(id=DEV_GUILD)
+        synced = await client.tree.sync(guild=guild)
+        print(f'Se han sincronizado {len(synced)} salsh commands')
+    except Exception as e:
+        print(f'[ERROR] No se han podido sincronizar los comandos: {e}')
 
-# Detecta los mensajes y filtra los comandos
-@client.event
-async def on_message(message: Message) -> None:
-    if not message.content or message.author.bot:
-        return
-    if message.content[0] == PREFIX:
-        await get_response(message, message.content)
+@client.tree.command(name='test1', description='hace un test', guild=GUILD_ID)
+async def caca(interaction: discord.Interaction):
+    await interaction.response.send_message('una prueba')
 
-# Inicia el bot con el token en .env
-def main() -> None:
-    client.run(token=TOKEN)
+@client.command(name='test2', aliases=['t2'])
+async def pis(ctx):
+    await ctx.send('dos prueba')
 
-if __name__ == '__main__':
-    main()
+client.run(token=TOKEN)
