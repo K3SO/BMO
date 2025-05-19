625e4fce6ba1e389020c1b7c5adaf4c96b0e4f08
K3SO <sergulaz05@gmail.com>
2025-05-19 12:40:47 +0200

    chore: limpiar el código, eliminar inutil

--- src/main.py
+++ src/main.py
@@ -8,30 +8,14 @@ from typing import Final
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 PREFIX: Final[str] = os.getenv('PREFIX')
-DEV_GUILD: Final[int] = os.getenv('DEV_GUILD')
+GUILD_ID = discord.Object(id=os.getenv('DEV_GUILD'))
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = commands.Bot(command_prefix=PREFIX, intents=intents)
-GUILD_ID = discord.Object(id=DEV_GUILD)
 
 @client.event
 async def on_ready():
     print(f'Bot conectado como {client.user}!\nPrefijo establecido como: "{PREFIX}"')
-    try:
-        guild = discord.Object(id=DEV_GUILD)
-        synced = await client.tree.sync(guild=guild)
-        print(f'Se han sincronizado {len(synced)} salsh commands')
-    except Exception as e:
-        print(f'[ERROR] No se han podido sincronizar los comandos: {e}')
-
-@client.tree.command(name='test1', description='hace un test', guild=GUILD_ID)
-async def caca(interaction: discord.Interaction):
-    await interaction.response.send_message('una prueba')
-
-@client.command(name='test2', aliases=['t2'])
-async def pis(ctx):
-    await ctx.send('dos prueba')
 
 client.run(token=TOKEN)
