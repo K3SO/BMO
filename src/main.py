34b127d1460c10d6b881f6d94ae34dd09ee547e7
K3SO <sergulaz05@gmail.com>
2025-06-13 16:31:45 +0200

    fix: cambiar "GUILD_ID" de srt a int, eliminar Final

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from typing import Final
 import sqlite3
 
 import command_handler
@@ -10,10 +9,9 @@ import message_handler
 
 # Declara las variables de .env
 load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-PREFIX: Final[str] = os.getenv('PREFIX')
-GUILD = discord.Object(id=os.getenv('DEV_GUILD'))
-GUILD_ID: Final[int] = os.getenv('DEV_GUILD')
+TOKEN = os.getenv('DISCORD_TOKEN')
+PREFIX = os.getenv('PREFIX')
+GUILD_ID = int(os.getenv('DEV_GUILD'))
 
 intents = discord.Intents.default()
 intents.message_content = True
